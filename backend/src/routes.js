8d8d16900766da1a8f20aa4f37e5751a332b32ae
drd-2rcl <drd.rclrd@gmail.com>
2020-03-26 09:57:05 -0300

    Route to create ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const crypto = require("crypto");
+const connection = require("./database/connection");
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ msg: "Hi" });
+routes.post("/ongs", async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return res.json({
+    id
+  });
 });
 
 module.exports = routes;
