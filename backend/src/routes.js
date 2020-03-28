058efbeecfe00a506e1033555003698a620eda8e
drd-2rcl <drd.rclrd@gmail.com>
2020-03-28 18:50:56 -0300

    Create route to sessions

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,11 @@ const express = require("express");
 const OngController = require("./controllers/ong_controller");
 const IncidentsController = require("./controllers/incident_controller");
 const ProfileController = require("./controllers/profile_controller");
+const SessionController = require("./controllers/session_controller");
 
 const routes = express.Router();
+
+routes.post("/sessions", SessionController.create);
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
