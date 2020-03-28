dc20d3313526f7c787032c0d0ea274b555851ae6
drd-2rcl <drd.rclrd@gmail.com>
2020-03-28 17:41:20 -0300

    Create routes to create and list incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const OngController = require("./controllers/ong_controller");
-
+const IncidentsController = require("./controllers/incident_controller");
 const routes = express.Router();
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
+
+routes.get("/incidents", IncidentsController.index);
+routes.post("/incidents", IncidentsController.create);
 
 module.exports = routes;
