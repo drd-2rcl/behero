164bc137e05ed9c6b183448109ac7150deae8f19
drd-2rcl <drd.rclrd@gmail.com>
2020-03-29 15:52:16 -0300

    Add logic to set id and save value in local storage

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,18 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
-import "./styles.css";
+
 import heroesImg from "../../assets/heroes.png";
 import logoImg from "../../assets/logo.svg";
+import api from "../../services/api";
+import "./styles.css";
 
 export default function Logon() {
+  const [id, setId] = useState("");
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("sessions", { id });
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
+      history.push("/profile");
+    } catch (error) {
+      alert("Falha no login tente novamente.");
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Heroe" />
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu login</h1>
-          <input placeholder="Seu ID" />
+          <input
+            value={id}
+            onChange={e => setId(e.target.value)}
+            placeholder="Seu ID"
+          />
           <button className="button" type="submit">
             Entrar
           </button>
