8143db9c07ca8b6e59bdf5fe2a44ca4bb2ffccfd
drd-2rcl <drd.rclrd@gmail.com>
2020-03-29 13:46:41 -0300

    Add structure to page new incident

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+
+import logoImg from "../../assets/logo.svg";
 
 import "./styles.css";
 
 export default function NewIncident() {
-  return <div>NewIncident</div>;
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Heroe" />
+          <h1>Cadastrar novo caso</h1>
+          <p>
+            Descreva o caso detalhamente para encontrar um herói para resolver
+            isso.
+          </p>
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voltar para home
+          </Link>
+        </section>
+        <form action="">
+          <input placeholder="Título do caso" />
+          <textarea placeholder="Descrição" />
+          <input placeholder="WhatsApp" />
+          <input placeholder="Valor em reais" />
+
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
